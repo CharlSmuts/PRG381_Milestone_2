52d17cfad1650ec4b3398bf0bff1567f783c60f8
Charl Smuts <charlsmuts12@gmail.com>
2025-07-15 15:10:11 +0200

    added code to drop tables, updated table creation

--- src/wellness/model/DBConnection.java
+++ src/wellness/model/DBConnection.java
@@ -50,13 +50,16 @@ public class DBConnection {
         System.out.println("creating tables");
             try 
                 {
-                    String Query1 = "CREATE TABLE appointments(student varchar(20), counselor varchar(20), date date, time time , status varchar(10))";
+                    String Query1 = "CREATE TABLE appointments(aid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), student VARCHAR(100) NOT NULL, "
+                            +  "counselor VARCHAR(100) NOT NULL, appointment_date DATE NOT NULL, appointment_time TIME NOT NULL,  status VARCHAR(50) NOT NULL)";
+                    
                     this.con.createStatement().execute(Query1);
 
-                    String Query2 = "CREATE TABLE counselors(name varchar(20), specialization varchar(20), available boolean)";
+                    String Query2 = "CREATE TABLE counselors(cid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), name VARCHAR(100), "+
+                            "specialization VARCHAR(100), available BOOLEAN)";
                     this.con.createStatement().execute(Query2);
 
-                    String Query3 = "CREATE TABLE feedback(student varchar(20), comments varchar(100))";
+                    String Query3 = "CREATE TABLE feedback(fid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), student VARCHAR(100), comments varchar(100))";
                     this.con.createStatement().execute(Query3);                  
                 }
         catch (SQLException e)
@@ -71,6 +74,20 @@ public class DBConnection {
         DatabaseMetaData dbm = con.getMetaData(); // get database info from connection
         try
         {
+            boolean deletetables = false; // set to true to recreate tables
+            
+            if (deletetables)
+            {
+               String Q1 = "DROP TABLE appointments";
+                String Q2 = "DROP TABLE counselors";
+                String Q3 = "DROP TABLE feedback";   
+                this.con.createStatement().execute(Q1);
+                this.con.createStatement().execute(Q2);
+                this.con.createStatement().execute(Q3);
+                System.out.println("Tables deleted"); 
+            }
+            
+            
             ResultSet rs = dbm.getTables(null, null, "APPOINTMENTS", null); //gets the appointments table from the database
             exists = rs.next(); // checks if the table exists and if it does sets exists to true. We only check 1 table because if 1 exists all of them will exist
         }
